455fb8b1ac242789dd0f41298c2f08e0b1947a2e
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-05-19 19:33:43 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,7 +18,6 @@ const Footer = () => {
                 <div className="col s12 l6 about">
                     <p>O Wealth Bank of Switzerland é uma empresa fictícia criada para contribuir com o cenário interpretativo do Advanced Roleplay (https://advanced-roleplay.com.br) sem fins lucrativos.</p>
                     <p>Qualquer semelhança com a realidade é pura coincidência e definitivamente não proposital.</p>
-                    <p>© Wealth Bank of Switzerland & Co. 2020</p>
                 </div>
                 <div className="col s12 l3 push-l3 links">
                     <div className="row">
@@ -33,12 +32,18 @@ const Footer = () => {
                                 <img src={LogoWbs} alt="WBS" />
                             </div>
                         </div>
-                        <div className="row">
-                            <p>Idealizado e desenvolvido por<br />Victor Ferraresi e Vitor Vasconcellos</p>
-                        </div>
                     </div>
                 </div>
             </div>
+            <div className="row">
+                <div className="col s12 l6 copyright">
+                    <p>© Wealth Bank of Switzerland & Co. 2020</p>
+                </div>
+                <div className="col s12 l3 push-l3 credits">
+                    <p>Idealizado e desenvolvido por<br />Victor Ferraresi e Vitor Vasconcellos</p>
+                </div>
+            </div>
+
             <div className="top">
                 <TopIcon onClick={scrollToTop} />
             </div>
