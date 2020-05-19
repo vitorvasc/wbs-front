421fa8ed2439ffc9f5427165072210b4113f75c6
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-05-19 00:21:31 -0300

    fixed header container width

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,27 +11,22 @@ const Header = () => (
     <div className="container">
       <div className="row">
         <MenuMobile />
-        
+
         <Link to="/">
           <div className="col s10 l2 logo">
             <img src={logo} alt="WBS" />
             <h5>WBS</h5>
           </div>
         </Link>
-        <div className="col l6 hide-on-med-and-down links">
+        <div className="col l7 hide-on-med-and-down links">
           <NavLink to="/quem-somos-nos" activeClassName="nav-selected">Quem somos nós</NavLink>
           <NavLink to="/carreiras" activeClassName="nav-selected">Carreiras</NavLink>
           <NavLink to="/midia" activeClassName="nav-selected">Mídia</NavLink>
           <NavLink to="/contato" activeClassName="nav-selected">Contato</NavLink>
         </div>
-        <div className="col l4 hide-on-med-and-down conta">
+        <div className="col l3 hide-on-med-and-down conta">
           <NavLink to="/minha-conta" activeClassName="nav-selected"><AccountIcon /> Acessar minha conta</NavLink>
         </div>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
-        </ul>
       </div>
     </div>
   </header>
