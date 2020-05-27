7b7ac637f8ae5e79c9dcbf6c67ae53fecd34b527
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-05-26 22:36:45 -0300

    added simple page loader

--- src/components/PageLoader/index.js
+++ src/components/PageLoader/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import './styles.scss';
 
+import gif from './assets/loader.gif';
+
+
 const PageLoader = () => {
-  return <span>TODO Loader...</span>;
+    return <div className="loader">
+        <img src={gif} alt="Carregando..."/>
+        <span>Aguarde, carregando...</span>
+    </div>;
 };
 
 export default PageLoader;
